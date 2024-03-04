5170a597238fe1eb328d63e4bebe8723ea600178
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-03-04 14:45:11 +0000

    012 - Add routes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Session;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +18,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home', ['title' => 'Home']);
+})->name('home');
+
+Route::prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::get('/register', 'register')->name('register');
+    Route::get('/login', 'login')->name('login');
 });
+
+Route::get('/crypto', function () {
+    return view('crypto.index');
+})->name('crypto');
+
+Route::get('/profile', [UserController::class, 'profile'])->name('profile');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
