9a9883ee4dcabeb231cba265df99eed7867994ce
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-03-04 15:00:21 +0000

    017 - Update filesystem config

--- config/filesystems.php
+++ config/filesystems.php
@@ -48,7 +48,7 @@ return [
 
         'public' => [
             'driver' => 'local',
-            'root' => storage_path('app/public'),
+            'root' => storage_path('public'),
             'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
             'throw' => false,
