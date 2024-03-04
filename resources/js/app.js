b884645a6b6c19831eaf39a0734756cc1f9300e4
Gilmar Brito dos Santos <gilmarbrito.dev@gmail.com>
2024-03-04 14:37:15 +0000

    010 - Update frontend resources

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
 import './bootstrap';
+
+import 'bootstrap';
+import flatpickr from "flatpickr";
+import * as echarts from 'echarts';
+import '../sass/app.scss'
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,38 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import path from 'path';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
+                'resources/template_assets/css/bootstrap.min.css',
+                'resources/template_assets/css/lineicons.css',
+                'resources/template_assets/css/materialdesignicons.min.css',
+                'resources/template_assets/css/main.css',
+                'resources/template_assets/js/bootstrap.bundle.min.js',
+                'resources/template_assets/js/polyfill.js',
+                'resources/template_assets/js/main.js',
+            ],
             refresh: true,
         }),
     ],
+    resolve: {
+        alias: {
+            '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+            'echarts': path.resolve(__dirname, 'node_modules/echarts'),
+        }
+    },
+    build: {
+        rollupOptions: {
+            external: [],
+            output: {
+                globals: {
+
+                }
+            }
+        }
+    }
 });
